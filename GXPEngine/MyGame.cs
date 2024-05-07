5706a98dbb3b4d8adfda707a25e5fe194fdf2ba0
Dylan Valkenaar <valkdylan@gmail.com>
2024-05-07 10:52:04 +0200

    oops vol. 2

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -5,19 +5,7 @@ using System.Drawing;                           // System.Drawing contains drawi
 public class MyGame : Game {
 	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
 		
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
 	}
 	
 	// For every game object, Update is called every frame, by the engine:
--- GXPEngine/RigidBody.cs
+++ GXPEngine/RigidBody.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GXPEngine;
+using GXPEngine.Managers;
 
-namespace GXPEngine
-{
-    internal class RigidBody
+public class RigidBody
     {
+    private Vec2 position;
+    private Vec2 velocity;
+    private Vec2 acceleration;
+
+    private RigidBody() { }
+    
+    public void Update() 
+        {
+        //Euler Integration
+        velocity += acceleration;
+        position += velocity;
+        }   
     }
-}
+
+
